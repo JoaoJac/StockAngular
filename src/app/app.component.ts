06caa9be9aadc3d967cc926bbb5400bafa097446
João Jacinto <joaoribeirojacinto@gmail.com>
2021-06-04 19:37:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exemploAngular';
+  ivas = [6,13,23];
+
+  productForm = new FormGroup({
+    price: new FormControl(''),
+    iva: new FormControl(''),
+    discount: new FormControl(''),
+  });
+
+  productValue = "";
+
+  constructor(
+    private http : HttpClient
+  ){}
+
+  setProductValue(product:any){
+    this.productValue = JSON.stringify(this.productForm.value);
+  }
+
+  createProduct(){
+    this.http.post("http://127.0.0.1:8080/stockMaven/api/products", this.productForm.value).subscribe(
+      (res:any) => this.setProductValue(res)
+    );
+  }
 }
